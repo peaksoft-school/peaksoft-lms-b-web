5f12b1601ddb994e3a245ed8020400d8000d8849
jetigen2003 <abdillaevjetigen127@gmail.com>
2022-04-27 18:59:16 +0600

    commit without changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import React from 'react'
-import { Notification } from './components/UI/Notification'
-import { ReactComponent as ErrorIcon } from './assets/icons/errorNotificationIcon.svg'
 
 function App() {
-   return (
-      <div>
-         <Notification
-            backColor="succes"
-            title="hello world "
-            icon={<ErrorIcon />}
-         />
-      </div>
-   )
+   return <div />
 }
 
 export default App
