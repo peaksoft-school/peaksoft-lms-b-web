ef4e06fdf4385f87c07f24a44e792039d04111eb
jetigen2003 <abdillaevjetigen127@gmail.com>
2022-04-27 20:44:16 +0600

    add breadcrumb layout and logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Routes, Route } from 'react-router'
+import BreadCrumb from './components/UI/BreadCrumb'
 
 function App() {
-   return <div />
+   return (
+      <div>
+         <Routes>
+            <Route path="/" element={<div>hello wordl</div>} />
+            <Route
+               path="/lessons/user/*"
+               element={
+                  <h2>
+                     <BreadCrumb />
+                  </h2>
+               }
+            />
+            <Route path="/lessons/:id" element={<div>lessons</div>} />
+         </Routes>
+      </div>
+   )
 }
 
 export default App
