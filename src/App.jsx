20e848049e7108411516d4f6c9ddf4f99475924c
Beybars <erkulow26@gmail.com>
2022-04-28 16:32:32 +0600

    removed unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import { Cards } from './components/UI/Cards'
 import { CustomDatePicker } from './components/UI/CustomDatePicker'
 
 function App() {
    return (
-      <div style={{ margin: '15px' }}>
+      <div>
          <CustomDatePicker />
-         <Cards />
       </div>
    )
 }
