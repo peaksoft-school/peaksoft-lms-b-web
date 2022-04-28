890f3a22731bce360ddf9368fcf0f7d8b4ef6211
jetigen2003 <abdillaevjetigen127@gmail.com>
2022-04-28 21:08:31 +0600

    instalation react-dropzzone and build ui imagePicker component

---
+++ src/components/UI/ImagePicker.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useCallback } from 'react'
+import styled from 'styled-components'
+import { useDropzone } from 'react-dropzone'
+import { ReactComponent as DropZone } from '../../assets/icons/DropZone.svg'
+import { Title } from './Title'
+
+const StyledImagePicker = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+
+   p {
+      text-align: center;
+      width: 240px;
+      font-family: var(--base-font-2);
+   }
+`
+
+const DropZoneWrapper = styled.div`
+   margin-bottom: 20px;
+`
+
+export const ImagePicker = () => {
+   const onDrop = useCallback((acceptedFiles) => {
+      console.log(acceptedFiles)
+   }, [])
+   const { getRootProps, getInputProps, isDragAccept, isDragReject } =
+      useDropzone({
+         onDrop,
+         multiple: false,
+         accept: 'image/jpeg,image/png',
+      })
+   return (
+      <StyledImagePicker>
+         <DropZoneWrapper {...getRootProps()}>
+            <input type="text" {...getInputProps()} />
+            <DropZone cursor="pointer" />
+         </DropZoneWrapper>
+         <Title color="#8d949e">
+            Нажмите на иконку чтобы загрузить или перетащите фото
+         </Title>
+      </StyledImagePicker>
+   )
+}
