763bcc48096499a1aa80c12e8cd1f463bbdeb0d7
Beybars <erkulow26@gmail.com>
2022-05-01 23:45:49 +0600

    change the structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,134 @@
 import React from 'react'
+import styled from 'styled-components'
+import Buttons from '@mui/material/IconButton/IconButton'
+import { Table } from './components/UI/Table'
+import { ReactComponent as EyeIcon } from './assets/icons/EyesForTable.svg'
+import { ReactComponent as EditIcon } from './assets/icons/EditIconForTable.svg'
+import { ReactComponent as TrashBinIcon } from './assets/icons/TrashBinForTable.svg'
 
+const WrapperIcons = styled.div`
+   width: 20vh;
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   & > * {
+      &:hover {
+         cursor: pointer;
+         color: blue;
+      }
+   }
+`
 function App() {
-   return <div>app</div>
+   const DATA = [
+      {
+         ID: 1,
+         'Имя Фамилия': 'Anna Karimova ',
+         Специализация: 'Онлайн',
+         'Номер телефона': '0777114676	',
+         'E-mail': 'John@gmail.com',
+         Пароль: 'tr56177ytu',
+      },
+      {
+         ID: 1,
+         'Имя Фамилия': 'Anna Karimova ',
+         Специализация: 'Онлайн',
+         'Номер телефона': '0777114676	',
+         'E-mail': 'John@gmail.com',
+         Пароль: 'tr56177ytu',
+      },
+   ]
+   const APPOINT_TEACHER = [
+      {
+         ID: 1,
+         'Имя Фамилия': 'Anna Karimova ',
+         Специализация: 'Онлайн',
+         'Номер телефона': '0777114676	',
+         'E-mail': 'John@gmail.com',
+      },
+      {
+         ID: 1,
+         'Имя Фамилия': 'Anna Karimova ',
+         Специализация: 'Онлайн',
+         'Номер телефона': '0777114676	',
+         'E-mail': 'John@gmail.com',
+      },
+   ]
+   const STUDENTS = [
+      {
+         ID: 1,
+         'Имя Фамилия': 'Anna Karimova',
+         Группа: 'JS-15-22',
+         Формат: 'Онлайн',
+         'Номер телефона': '0777114676',
+         'E-mail': 'John@gmail.com',
+      },
+      {
+         ID: 1,
+         'Имя Фамилия': 'Anna Karimova',
+         Группа: 'JS-15-22',
+         Формат: 'Онлайн',
+         'Номер телефона': '0777114676',
+         'E-mail': 'John@gmail.com',
+      },
+   ]
+   const INSTRUKTOR_CREATE_STUDEN = [
+      {
+         ID: 1,
+         'Имя Фамилия': 'Anna Karimova ',
+         Группа: 'JS-15-22',
+         'Формат обучения': 'Онлайн',
+         'Номер телефона': '0777114676	',
+         'E-mail': 'John@gmail.com',
+         Пароль: 'tr56177ytu',
+      },
+      {
+         ID: 1,
+         'Имя Фамилия': 'Anna Karimova ',
+         Группа: 'JS-15-22',
+         'Формат обучения': 'Онлайн',
+         'Номер телефона': '0777114676	',
+         'E-mail': 'John@gmail.com',
+         Пароль: 'tr56177ytu',
+      },
+   ]
+   const data = DATA.map((newKey) => ({
+      ...newKey,
+      Действия: (
+         <WrapperIcons>
+            <Buttons
+               onClick={() => alert('Eye')}
+               style={{ background: 'none' }}
+            >
+               <EyeIcon />
+            </Buttons>
+            <Buttons
+               onClick={() => alert('Edit')}
+               style={{ background: 'none' }}
+            >
+               <EditIcon />
+            </Buttons>
+            <Buttons
+               onClick={() => alert('Trash')}
+               style={{ background: 'none' }}
+            >
+               <TrashBinIcon />
+            </Buttons>
+         </WrapperIcons>
+      ),
+   }))
+   const instructorCreateStudens = INSTRUKTOR_CREATE_STUDEN.map((newkey) => ({
+      ...newkey,
+      Удалить: (
+         <Buttons
+            onClick={() => alert('Remove')}
+            style={{ background: 'none' }}
+         >
+            <TrashBinIcon />
+         </Buttons>
+      ),
+   }))
+
+   return <Table data={data} />
 }
 
 export default App
--- src/components/UI/Table.jsx
+++ src/components/UI/Table.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
-import Buttons from '@mui/material/IconButton/IconButton'
-import { ReactComponent as EyeIcon } from '../../assets/icons/EyesForTable.svg'
-import { ReactComponent as EditIcon } from '../../assets/icons/EditIconForTable.svg'
-import { ReactComponent as TrashBinIcon } from '../../assets/icons/TrashBinForTable.svg'
 
-const WrapperIcons = styled.div`
-   width: 20vh;
-   display: flex;
-   justify-content: space-between;
-   align-items: center;
-   & > * {
-      &:hover {
-         cursor: pointer;
-         color: blue;
-      }
-   }
-`
+export function Table({ data }) {
+   const columns = data[0] && Object.keys(data[0])
+   const headerOfTable = data[0] && columns.map((heading) => <th>{heading}</th>)
+   const bodyOfTable = data.map((row) => (
+      <tr>
+         {columns.map((column) => (
+            <td>{row[column]}</td>
+         ))}
+      </tr>
+   ))
+   return (
+      <WraperForTable>
+         <StyledTable cellPadding={0} cellSpacing={0}>
+            <Thead>
+               <tr>{headerOfTable}</tr>
+            </Thead>
+            <Tbody>{bodyOfTable}</Tbody>
+         </StyledTable>
+      </WraperForTable>
+   )
+}
 const WraperForTable = styled.div`
-   max-width: 170vh;
-   min-width: 128vh;
    min-height: 86.3vh;
    min-height: 94.5vh;
    background-color: #fff;
@@ -29,6 +32,7 @@ const StyledTable = styled.table`
    background-color: #eff0f6;
    font-family: var(--base-font);
    font-style: normal;
+   width: 170vh;
    color: #1d293f;
    hr {
       border-top: 0.2px solid #e7e9eb;
@@ -38,7 +42,8 @@ const Thead = styled.thead`
    font-weight: 400;
    font-size: 14px;
    line-height: 19px;
-   .tr,
+   width: 170vh;
+
    th {
       white-space: nowrap;
       padding: 1rem;
@@ -66,52 +71,3 @@ const Tbody = styled.tbody`
       padding: 10px 30px 10px 20px;
    }
 `
-export function Table({ userData, onVeiwPassword, onEdit, onDelete }) {
-   const onVeiwPasswordHandler = () => {
-      onVeiwPassword()
-   }
-   const onEditHandler = () => {
-      onEdit()
-   }
-   const onDeleteHdandler = () => {
-      onDelete()
-   }
-   const data = userData.map((newKey) => ({
-      ...newKey,
-      Действия: (
-         <WrapperIcons>
-            <Buttons
-               onClick={onVeiwPasswordHandler}
-               style={{ background: 'none' }}
-            >
-               <EyeIcon />
-            </Buttons>
-            <Buttons onClick={onEditHandler} style={{ background: 'none' }}>
-               <EditIcon />
-            </Buttons>
-            <Buttons onClick={onDeleteHdandler} style={{ background: 'none' }}>
-               <TrashBinIcon />
-            </Buttons>
-         </WrapperIcons>
-      ),
-   }))
-   const columns = data[0] && Object.keys(data[0])
-   const headerOfTable = data[0] && columns.map((heading) => <th>{heading}</th>)
-   const bodyOfTable = data.map((row) => (
-      <tr>
-         {columns.map((column) => (
-            <td>{row[column]}</td>
-         ))}
-      </tr>
-   ))
-   return (
-      <WraperForTable>
-         <StyledTable cellPadding={0} cellSpacing={0}>
-            <Thead>
-               <tr>{headerOfTable}</tr>
-            </Thead>
-            <Tbody>{bodyOfTable}</Tbody>
-         </StyledTable>
-      </WraperForTable>
-   )
-}
