dac8ec9e3e056b4c881066773d4bb2f3c4526aba
Orozdaan <Orozdaan19@gmail.com>
2022-05-03 18:51:08 +0600

    change to reausable component

--- src/components/UI/BasicSelect.jsx
+++ src/components/UI/BasicSelect.jsx
@@ -1,42 +1,45 @@
 import * as React from 'react'
-import Box from '@mui/material/Box'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
 import styled from 'styled-components'
 import { InputLabel } from '@mui/material'
 
-export function BasicSelect({ data, placeholder }) {
+export function BasicSelect({ data, placeholder, onChoose }) {
    const [value, setValue] = React.useState('')
    const handleChange = (event) => {
       setValue(event.target.value)
+
+      onChoose({
+         ...event.target.value,
+      })
    }
    return (
-      <Box>
-         <FormControlForSelect>
-            <InputLabel id="demo-simple-select-label">{placeholder}</InputLabel>
-            <Select
-               labelId="demo-simple-select-label"
-               id="demo-simple-select"
-               value={value}
-               label={placeholder}
-               onChange={handleChange}
-               InputLabelProps={{ shrink: false }}
-            >
-               {data.map((el) => (
-                  <MenuItem key={el.id} value={el.title}>
-                     {el.title}
-                  </MenuItem>
-               ))}
-            </Select>
-         </FormControlForSelect>
-      </Box>
+      <FormControlForSelect>
+         <InputLabel sx={{ top: '-5px' }} id="demo-simple-select-label">
+            {placeholder}
+         </InputLabel>
+         <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={value}
+            label={placeholder}
+            onChange={handleChange}
+            // InputLabelProps={{ shrink: false }}
+         >
+            {data.map((el) => (
+               <MenuItem key={el.id} value={el}>
+                  {el.title}
+               </MenuItem>
+            ))}
+         </Select>
+      </FormControlForSelect>
    )
 }
 const FormControlForSelect = styled(FormControl)`
    .MuiInputBase-root {
       border-radius: 10px;
-      height: 42px;
       width: 491px;
+      height: 42px;
    }
 `
