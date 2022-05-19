acc5e7c802b028552f6261d74077da1760e56cb6
jetigen2003 <abdillaevjetigen127@gmail.com>
2022-05-19 20:48:02 +0600

    save

--- src/routes/ADMIN_ROUTES/CoursesRoute.jsx
+++ src/routes/ADMIN_ROUTES/CoursesRoute.jsx
@@ -1,13 +1,9 @@
-import React, { useEffect } from 'react'
-import { Routes, Route, useSearchParams } from 'react-router-dom'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import { CoursePanel } from '../../components/ADMIN/courses/CoursePanel'
 import { CourseInnerPage } from '../../components/ADMIN/courses/CourseInnerPage'
 
 export const CoursesRoutes = () => {
-   const [searchParams, setSearchParams] = useSearchParams()
-   useEffect(() => {
-      setSearchParams({ page: 1 })
-   }, [])
    return (
       <Routes>
          <Route path="/" element={<CoursePanel />} />
