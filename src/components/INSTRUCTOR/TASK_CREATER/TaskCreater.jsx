b08d35edb26774feecd25ade30742d1615c678d5
jetigen2003 <abdillaevjetigen127@gmail.com>
2022-05-30 18:26:15 +0600

    save

--- src/components/INSTRUCTOR/TASK_CREATER/TaskCreater.jsx
+++ src/components/INSTRUCTOR/TASK_CREATER/TaskCreater.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 import { BreadCrumb } from '../../UI/BreadCrumb'
 import { TextEditor } from '../TEXT_EDITOR/TextEditor'
 import { Inputs } from '../../UI/Input'
-import { Buttons } from '../../UI/Buttons'
 import { ReactComponent as Text } from '../../../assets/icons/Text.svg'
 import { ReactComponent as FileEditor } from '../../../assets/icons/FileEditor.svg'
 import { ReactComponent as ImageEditor } from '../../../assets/icons/ImageEditor.svg'
@@ -17,14 +16,27 @@ export const TaskCreater = () => {
          <StyledTaskCreater>
             <Header>Создать задание</Header>
             <ControlMenu>
-               <Inputs width="1200px" />
-               <Text />
-               <FileEditor />
-               <ImageEditor />
-               <LinkEditor />
-               <CodeEditor />
+               <Inputs width="2000px" />
+               <EditorButtons>
+                  <Text />
+               </EditorButtons>
+               <EditorButtons>
+                  <FileEditor />
+               </EditorButtons>
+               <EditorButtons>
+                  <ImageEditor />
+               </EditorButtons>
+               <EditorButtons>
+                  <LinkEditor />
+               </EditorButtons>
+               <EditorButtons>
+                  <CodeEditor />
+               </EditorButtons>
             </ControlMenu>
-            <TextEditor isHasEditor />
+            <StyledTasksWrapper>
+               <TextEditor isHasEditor />
+               <TextEditor />
+            </StyledTasksWrapper>
          </StyledTaskCreater>
       </Wrapper>
    )
@@ -54,4 +66,26 @@ const ControlMenu = styled.div`
    display: grid;
    grid-template-columns: 2.5fr 0.1fr 0.1fr 0.1fr 0.1fr 0.1fr;
    align-items: center;
+`
+const EditorButtons = styled.button`
+   background-color: transparent;
+   opacity: 0.7;
+   border-radius: 6px;
+   margin-left: 15px;
+   outline: none;
+   border: none;
+   transition: 0.6s;
+   &:hover {
+      box-shadow: 0 0 4px black;
+   }
+   &:active {
+      box-shadow: 0 0 8px black;
+   }
+`
+
+const StyledTasksWrapper = styled.div`
+   border: 1px solid #d4d4d4;
+   border-radius: 10px;
+   padding: 15px;
+   margin-top: 25px;
 `
